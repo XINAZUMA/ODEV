0fddc53fe44a067d0dd17ccc8182644f45b634f0
XINAZUMA <117636430+XINAZUMA@users.noreply.github.com>
2023-01-09 14:04:58 +0300

    Add files via upload

---
+++ ödevvv/ödevvv/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödevvv
+{
+    public partial class Form2 : Form
+    {
+        Form3 form3 = new Form3();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                MessageBox.Show("BAŞARI TESTİ AÇILIYOR", "Onay Verildi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                form3.Show();
+                this.Hide();
+            }
+        }
+    }
+}
